1201cc412c17b643669b5e2fd429307f5bf0a112
matahho <mata1381@gmail.com>
2024-03-07 22:16:01 +0330

    add comments crawler

---
+++ comments/management/commands/comments.py
@@ -0,0 +1,67 @@
+from django.core.management.base import BaseCommand
+from comments.models import Comment
+import instaloader
+from os import environ
+try:
+    from instaloader import ConnectionException, Instaloader
+except ModuleNotFoundError:
+    raise SystemExit("Instaloader not found.\n  pip install [--user] instaloader")
+
+
+class Command(BaseCommand):
+    help = (
+        'Save all commands from a post '
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument('-p', '--post', type=str, help='url of the post')
+
+    @staticmethod
+    def get_post_id_from_url(url):
+        parsed = url.split('/')
+        post_id_index = parsed.index('p') + 1
+        return parsed[post_id_index]
+
+    @staticmethod
+    def get_answers(comment):
+        answers = {}
+        for answer in comment.answers:
+            answers[answer.owner.username] = answer.text
+        return answers
+
+
+    def handle(self, *args, **options):
+        post_url = options['post']
+        post_id = self.get_post_id_from_url(post_url)
+        print(environ.get('INSTAGRAM_SESSION_USERNAME'))
+
+        try:
+            loader = Instaloader()
+            loader.load_session_from_file(environ.get('INSTAGRAM_SESSION_USERNAME'))
+            post = instaloader.Post.from_shortcode(loader.context, post_id)
+
+            for comment in post.get_comments():
+                Comment.objects.create(
+                    id=comment.id,
+                    username=comment.owner.username,
+                    created=comment.created_at_utc,
+                    text=comment.text,
+                    likes=comment.likes_count,
+                    answers=self.get_answers(comment),
+                    post_id=post_id
+                )
+
+            self.stdout.write(self.style.SUCCESS('Comments saved successfully.'))
+        except ConnectionException as e:
+            raise ConnectionError(f"Connection error: {e}")
+
+
+
+
+
+
+
+
+
+
+
--- comments/models.py
+++ comments/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Comment (models.Model):
+    id = models.BigIntegerField(primary_key=True)
+    created = models.DateTimeField()
+    username = models.CharField(max_length=255)
+    text = models.TextField()
+    likes = models.IntegerField(default=0)
+    answers = models.JSONField()
+    post_id = models.CharField(max_length=255 , default=None)
